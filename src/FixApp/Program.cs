ad87d3a9c0325d42d651d5f7aa8b9b1a4e2c056c
Pedro Felix <pmhsfelix@gmail.com>
2013-12-01 23:03:05 +0000

    Changes the Fix sample to allow a continuation middleware after Web API

--- src/FixApp/Program.cs
+++ src/FixApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -22,6 +24,24 @@ namespace FixApp
             };
         }
     }
+
+    class AdapterMiddleware : OwinMiddleware
+    {
+        private readonly Func<IDictionary<string, object>, Task> _next;
+        public AdapterMiddleware(Func<IDictionary<string,object>,Task> next) :
+            // According to the source code, the OwinMiddleware.Next can be null
+            // This middleware next element is not a OwinMiddleware
+            base(null) 
+        {
+            _next = next;
+        }
+
+        public override Task Invoke(IOwinContext context)
+        {
+            return _next.Invoke(context.Environment);
+        }
+    }
+
     class Program
     {
         // Based on http://blog.markrendle.net/2013/10/01/fix-and-owin-and-simple-web/
@@ -30,11 +50,26 @@ namespace FixApp
             var config = new HttpConfiguration();
             config.Routes.MapHttpRoute("ApiDefault", "{controller}/{id}", new {id = RouteParameter.Optional});
             var server = new HttpServer(config);
-            var handler = new HttpMessageHandlerAdapter(null, server, new OwinBufferPolicySelector());
             
             // Build the OWIN app
             var app = new Fixer()
-                .Use((ctx, next) => handler.Invoke(new OwinContext(ctx)))
+                // I'm creating a new handler *for each* request
+                // because the next is defined in the HttpMessageHandlerAdapter ctor,
+                .Use((ctx, next) => 
+                    new HttpMessageHandlerAdapter(
+                        new AdapterMiddleware(next), 
+                        server, 
+                        new OwinBufferPolicySelector())
+                            .Invoke(new OwinContext(ctx)))
+
+                // Final middleware to return a 404
+                .Use(async (ctx, next) =>
+                {
+                    var kctx = new OwinContext(ctx);
+                    kctx.Response.StatusCode = 404;
+                    kctx.Response.Headers.Append("Content-Type", "text/plain");
+                    await kctx.Response.WriteAsync("No fx has the resource identified by the request");
+                })
                 .Build();
 
             // Set up the Nowin server
